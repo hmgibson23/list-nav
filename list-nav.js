fe2bcf1cc7344b8ba4b0e78b246696ec56a24e5c
Hugo Gibson <hugo.gibson@roh.org.uk>
2013-08-21 11:30:42 +0100

    Changed to use protoypes

--- list-nav.js
+++ list-nav.js
@@ -1,148 +1,181 @@
-/*Remove the passed element from the DOM, and reinsert it later*/
+(function(window, undefined) {
+    var LN = function(ul) {
+        this.ul = ul;
+    };
 
-function removeAndInsert(el) {
-    var parentNode = el.parentNode;
-    var nextSibling = el.nextSibling;
-    parentNode.removeChild(el);
-    return function() {
-        if(nextSibling) {
-            parentNode.insertBefore(el, nextSibling);
-        } else {
-            parentNode.appendChild(el);
+    LN.prototype = {
+        start: function() {
+            var list = this.ul.getElementsByTagName('li');
+            //first assign all of the letters
+            for (var i = 0, ii = list.length; i < ii; i++) {
+                this.assignLetter(list[i]);
+            }
+
+            var navName = this.ul.id + '-nav';
+
+            var links = document.getElementById(navName).getElementsByTagName('a');
+
+            var ul = this.ul; //alias for simplicity
+
+            for (var i = 0, ii = links.length; i < ii; i++) {
+                links[i].onclick = function(event) {
+                    var evt = window.event || event; //event standadisation
+                    evt.preventDefault ? evt.preventDefault() : evt.returnValue = false; //IE8 does not support event.preventDefault() - thanks Microsoft.
+                    var link_val = this.innerHTML.toLowerCase();
+                    LN.prototype.showHide(link_val, ul);
+                };
+            }
+            //create and bind the 'All' link
+            var li = document.createElement('li');
+            var a = document.createElement('a');
+            a.setAttribute('id', 'all-link');
+            a.setAttribute('href', '#/all');
+            a.innerHTML = "All";
+            li.appendChild(a);
+            document.getElementById(navName).insertBefore(li, document.getElementById(navName).firstChild);
+            var that = this; //alias
+            document.getElementById('all-link').onclick = function() {
+                var insertFunction = that.removeAndInsert(ul);
+                var to_show = ul.getElementsByTagName('li');
+                for (var i = 0, ii = to_show.length; i < ii; i++) {
+                    to_show[i].style.display = 'inline-block';
+                }
+                insertFunction();
+            };
+
+
+        },
+
+
+        showHide: function(letter, ul) {
+            var list = ul.getElementsByTagName('li');
+            for (var i = 0, ii = list.length; i < ii; i++) {
+                list[i].style.display = 'none';
+            }
+
+            //show the li that match the letter
+            var search = '.' + letter;
+            var to_show = document.querySelectorAll(search);
+            var insertFunction = this.removeAndInsert(ul);
+            for (var i = 0, ii = to_show.length; i < ii; i++) {
+                to_show[i].style.display = 'inline-block';
+            }
+            insertFunction();
+        },
+
+
+        removeAndInsert: function(el) {
+            var parentNode = el.parentNode;
+            var nextSibling = el.nextSibling;
+            parentNode.removeChild(el);
+            return function() {
+                if (nextSibling) {
+                    parentNode.insertBefore(el, nextSibling);
+                } else {
+                    parentNode.appendChild(el);
+                }
+            };
+        },
+
+        assignLetter: function(el) {
+            //uses a hash table top assign letters to all of the li in the ul so that they can be sorted.
+            //hash tables are much more efficient than switch statements in JavaScript
+            var first = el.getElementsByTagName('a')[0].innerHTML.charAt(0).toLowerCase();
+            var cases = {};
+            cases['a'] = function() {
+                el.className += ' a';
+            };
+            cases['b'] = function() {
+                el.className += ' b';
+            };
+            cases['c'] = function() {
+                el.className += ' c';
+            };
+            cases['d'] = function() {
+                el.className += ' d';
+            };
+            cases['e'] = function() {
+                el.className += ' e';
+            };
+            cases['f'] = function() {
+                el.className += ' f';
+            };
+            cases['g'] = function() {
+                el.className += ' g';
+            };
+            cases['h'] = function() {
+                el.className += ' h';
+            };
+            cases['i'] = function() {
+                el.className += ' i';
+            };
+            cases['j'] = function() {
+                el.className += ' j';
+            };
+            cases['k'] = function() {
+                el.className += ' k';
+            };
+            cases['l'] = function() {
+                el.className += ' l';
+            };
+            cases['m'] = function() {
+                el.className += ' m';
+            };
+            cases['n'] = function() {
+                el.className += ' n';
+            };
+            cases['o'] = function() {
+                el.className += ' o';
+            };
+            cases['p'] = function() {
+                el.className += ' p';
+            };
+            cases['q'] = function() {
+                el.className += ' q';
+            };
+            cases['r'] = function() {
+                el.className += ' r';
+            };
+            cases['s'] = function() {
+                el.className += ' s';
+            };
+            cases['t'] = function() {
+                el.className += ' t';
+            };
+            cases['u'] = function() {
+                el.className += ' u';
+            };
+            cases['v'] = function() {
+                el.className += ' v';
+            };
+            cases['w'] = function() {
+                el.className += ' w';
+            };
+            cases['x'] = function() {
+                el.className += ' x';
+            };
+            cases['y'] = function() {
+                el.className += ' y';
+            };
+            cases['z'] = function() {
+                el.className += ' z';
+            };
+
+            if (typeof cases[first] == 'function') {
+                cases[first]();
+            }
         }
-    }
-}
+        //end prototype
+    };
 
-/*removes a class from an element*/
 
-function removeClass(el, cls) {
-    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-    el.className = el.className.replace(reg, ' ');
-}
 
-function ULSort(ul) {
-    var list = ul.getElementsByTagName('li');
-    //first assign all of the letters
-    for(var i = 0, ii = list.length; i < ii; i++) {
-        assignLetter(list[i]);
-    }
-    //now make an alphabetical list
-    var navName = ul.id + '-nav';
-    document.getElementById(navName).innerHTML = '<ul><li><a href="#">A</a></li><li><a href="#">B</a></li><li><a href="#">C</a></li><li><a href="#">D</a></li><li><a href="#">E</a></li><li><a href="#">F</a></li><li><a href="#">G</a></li><li><a href="#">H</a></li><li><a href="#">I</a></li><li><a href="#">J</a></li><li><a href="#">K</a></li><li><a href="#">L</a></li><li><a href="#">M</a></li><li><a href="#">N</a></li><li><a href="#">O</a></li><li><a href="#">P</a></li><li><a href="#">Q</a></li><li><a href="#">R</a></li><li><a href="#">S</a></li><li><a href="#">T</a></li><li><a href="#">U</a></li><li><a href="#">V</a></li><li><a href="#">W</a></li><li><a href="#">X</a></li><li><a href="#">Y</a></li><li><a href="#">Z</a></li></ul>';
-
-    var links = document.getElementById(navName).firstChild.getElementsByTagName('a');
-    for(var i = 0, ii = links.length; i < ii; i++) {
-        links[i].onclick = function(event) {
-            var link_val = this.innerHTML.toLowerCase();
-            showHide(link_val, ul);
+    var ListNav = {
+        init: function(ul) {
+            return new LN(ul);
         }
-    }
+    };
 
-}
+    window.ListNav = ListNav;
 
-function showHide(letter, ul) {
-    var list = ul.getElementsByTagName('li');
-    for(var i = 0, ii = list.length; i < ii; i++) {
-        removeClass(list[i], 'li-show');
-        removeClass(list[i], 'li-default');
-        list[i].className += ' li-default';
-    }
-
-    //show the li that match the letter
-    var search = '.' + letter;
-    var to_show = document.querySelectorAll(search);
-    var insertFunction = removeAndInsert(ul);
-    for(var i = 0, ii = to_show.length; i < ii; i++) {
-        to_show[i].className += ' li-show';
-    }
-    insertFunction();
-}
-
-function assignLetter(el) {
-    //uses a hash table top assign letters to all of the li in the ul so that they can be sorted.
-    //hash tables are much more efficient than switch statements in JavaScript
-    var first = el.firstChild.innerHTML[0].toLowerCase();
-    var cases = {};
-    cases['a'] = function() {
-        el.setAttribute('class', 'a');
-    }
-    cases['b'] = function() {
-        el.setAttribute('class', 'b');
-    }
-    cases['c'] = function() {
-        el.setAttribute('class', 'c');
-    }
-    cases['d'] = function() {
-        el.setAttribute('class', 'd');
-    }
-    cases['e'] = function() {
-        el.setAttribute('class', 'e');
-    }
-    cases['f'] = function() {
-        el.setAttribute('class', 'f');
-    }
-    cases['g'] = function() {
-        el.setAttribute('class', 'g');
-    }
-    cases['h'] = function() {
-        el.setAttribute('class', 'h');
-    }
-    cases['i'] = function() {
-        el.setAttribute('class', 'i');
-    }
-    cases['j'] = function() {
-        el.setAttribute('class', 'j');
-    }
-    cases['k'] = function() {
-        el.setAttribute('class', 'k');
-    }
-    cases['l'] = function() {
-        el.setAttribute('class', 'l');
-    }
-    cases['m'] = function() {
-        el.setAttribute('class', 'm');
-    }
-    cases['n'] = function() {
-        el.setAttribute('class', 'n');
-    }
-    cases['o'] = function() {
-        el.setAttribute('class', 'o');
-    }
-    cases['p'] = function() {
-        el.setAttribute('class', 'p');
-    }
-    cases['q'] = function() {
-        el.setAttribute('class', 'q');
-    }
-    cases['r'] = function() {
-        el.setAttribute('class', 'r');
-    }
-    cases['s'] = function() {
-        el.setAttribute('class', 's');
-    }
-    cases['t'] = function() {
-        el.setAttribute('class', 't');
-    }
-    cases['u'] = function() {
-        el.setAttribute('class', 'u');
-    }
-    cases['v'] = function() {
-        el.setAttribute('class', 'v');
-    }
-    cases['w'] = function() {
-        el.setAttribute('class', 'w');
-    }
-    cases['x'] = function() {
-        el.setAttribute('class', 'x');
-    }
-    cases['y'] = function() {
-        el.setAttribute('class', 'y');
-    }
-    cases['z'] = function() {
-        el.setAttribute('class', 'z');
-    }
-
-    if(typeof cases[first] == 'function') {
-        cases[first]();
-    }
-}
+})(window, undefined);
